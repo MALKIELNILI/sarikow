3c9089b5127faa45d9f767ee3f00996546cbcca9
Sarikow <sarikow@local.com>
2026-06-15 10:25:18 +0300

    אפשרות 'כל שנה' גם להגדרת בין הזמנים

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'sarikow-v20260615-1030';
+const CACHE = 'sarikow-v20260615-1100';
 const FILES = ['./','./index.html','./manifest.json','./icon.jpg'];
 
 self.addEventListener('install', e => {
